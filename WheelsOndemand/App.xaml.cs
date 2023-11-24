1e888056a28368d1bf6e624ab859a7f8b9c1d360
poojagithub <A00302457@mycambian.ca>
2023-11-24 13:13:56 -0700

    Add project files.

---
+++ WheelsOndemand/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace WheelsOndemand
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ WheelsOndemand/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace WheelsOndemand
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
