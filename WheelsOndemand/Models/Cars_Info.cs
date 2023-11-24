27ada4ee76ded0c487022ce8a1af358eb1ae6977
poojagithub <A00302457@mycambian.ca>
2023-11-24 14:59:56 -0700

    Created 2 models. Cara_Info.cs Represents information about each car, such as make, model, year, and availability. This model is essential for managing the fleet of cars.  User_Info Manages user information, including customer details. This model is crucial for user authentication, registration, and personalized experiences.

---
+++ WheelsOndemand/Models/Cars_Info.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WheelsOndemand.Models
+{
+    internal class Cars_Info
+    {
+       // public Cars_Info() { }
+        public string Car_Name { get; set; }
+        public string Car_Description { get; set; }
+        public int Model_Year { get; set; }
+        public Boolean Availibility { get; set; }
+
+
+    }
+}
---
+++ WheelsOndemand/Models/User_Info.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WheelsOndemand.Models
+{
+    internal class User_Info
+    {
+        public int Id { get; set; }
+        public string Password { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public string Address { get; set; }
+       
+    }
+}
