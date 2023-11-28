b9b0a091f1b94881ccb7c2138a9bb3bc4a858605
Gurkirpal Singh <A00287624@mycambrian.ca>
2023-11-28 09:27:20 -0700

    In our rental car app, the "Admin" class helps control access for administrative users by storing their names and a code. This ensures secure management. The "Invoice_Info" class simplifies invoice creation by storing details like names, description, pricing, and contact info, making the invoicing process smooth. Both classes improve how our rental car app works and stays organized.

---
+++ WheelsOndemand/Models/Admin.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WheelsOndemand.Models
+{
+    internal class Admin
+    {
+        public string First_Name { get; set; }
+        public string Last_Nmae { get; set; }
+        public string Code {  get; set; }
+    }
+}
---
+++ WheelsOndemand/Models/Invoive_Info.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WheelsOndemand.Models
+{
+    internal class Invoive_Info
+    {
+        public string First_Name { get; set; }
+        public string Last_Nmae { get; set; }
+        public string Description {  get; set; }
+        public float Price {  get; set; }
+        public float Tax {  get; set; }
+        public float Final_Price {  get; set; } 
+
+           public int Mobile_No { get; set; }
+        public string Email_Id {  get; set; }
+    }
+}
