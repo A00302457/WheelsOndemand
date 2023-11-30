61c333d797ddf62ba6d9514ed5fc2d6ce2d98ea1
poojagithub <A00302457@mycambian.ca>
2023-11-30 14:39:41 -0700

    Update 4 models as we discuss in check in meeting 2. We update 4 models as per your suggestions and added detaileds comment about our class. Thank you for your suggestions.

--- WheelsOndemand/Models/Cars_Info.cs
+++ WheelsOndemand/Models/Cars_Info.cs
@@ -6,13 +6,18 @@ using System.Threading.Tasks;
 
 namespace WheelsOndemand.Models
 {
+    //The class "Cars_Info" defines six properties: CarID, Car_Image, Car_Brand, Car_Model, Model_Year, and Price_Per_Day.
+    //These properties can be used to store information about the cars available for rent, such as their brand,
+    //model, year, image, and price per day.
     internal class Cars_Info
     {
-       // public Cars_Info() { }
-        public string Car_Name { get; set; }
-        public string Car_Description { get; set; }
+      
+        public int CarID {  get; set; }
+        public string Car_Image { get; set; }
+        public string Car_Brand { get; set; }
+        public string Car_Model { get; set; }
         public int Model_Year { get; set; }
-        public Boolean Availibility { get; set; }
+        public int Price_Per_Day { get; set; }
 
 
     }
---
+++ WheelsOndemand/Models/Payment_Card.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WheelsOndemand.Models
+{
+    //The Payment_Card class definition that can be used to store information about payment cards in my application.
+    //The class defines six properties: PaymentCard_Id, User_Id, number, Name, CVC, and Expiry_Date.
+   // By creating an instance of the Payment_Card class for each payment card in application,
+   // you can store all the relevant information about the payment card in one place.This can make it easier
+   // to manage your payment cards and keep track of their information.
+    internal class Payment_Card
+    {
+        public int PaymentCard_Id { get; set; }
+        public int User_Id {  get; set; }
+        public int number {  get; set; }
+        public string Name { get; set; }
+        public int CVC { get; set; }
+        public string Expiry_Date {  get; set; }
+    }
+}
