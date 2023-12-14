0834265a839774b04450c64e3f3a5c4026fad329
Gurkirpal Singh <A00287624@mycambrian.ca>
2023-12-13 21:08:40 -0700

    we had meeting with professor and We made some changes like We create the loginsession class that help the access loginsession any where in project, along wih that we created the admincarlistviewmodel , cardetailviewmodel classes , so admin can change details and can also ad the new cars in the add

--- WheelsOndemand/Services/AppSettings.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace WheelsOndemand.Services
-{
-   public static class AppSettings
-    {
-        public static string FirebaseApiKey => "AIzaSyBuaymMSVjdxe8wBtlIWGPRDki-ORo4lJU";
-        public static string FirebaseAuthDomain => "wheelsondemand-25efa.firebaseapp.com";
-    }
-}
